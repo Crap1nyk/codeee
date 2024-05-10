5d603bf6db984970577265882a17fa9a10b6afb8
crap1nyk <nv06645@gmail.com>
2024-05-11 03:38:55 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:part3/pages/ClothingHomePage.dart';
+import 'package:part3/pages/LoginScreen.dart';
+import 'package:part3/pages/SplashScreen.dart';
+import 'package:part3/pages/SignupScreen.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(ClothingApp());
 }
 
-class MyApp extends StatelessWidget {
+class ClothingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      title: 'Clothing App',
       theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        primaryColor: Colors.indigo[900],
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pages Example'),
-      ),
-      body: Column(
-        children: [
-          Expanded(child: ClothingHomePage()),
-        ],
-      ),
+      // Define initial route as Splash screen
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashScreen(), // Define route for SplashScreen
+        '/login': (context) => LoginScreen(), // Define route for LoginScreen
+        '/home': (context) => ClothingHomePage(), // Define route for ClothingHomePage
+        '/signup': (context) => SignupScreen(), 
+      },
     );
   }
 }
---
+++ lib/pages/SplashScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:part3/pages/ClothingHomePage.dart';
+import 'package:part3/pages/LoginScreen.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Simulate checking authentication status
+    bool isAuthenticated = false; // Replace with your authentication logic
+
+    Future.delayed(Duration(seconds: 2), () {
+      // Navigate to either login or home page based on authentication status
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => isAuthenticated ? ClothingHomePage() : LoginScreen(),
+      ));
+    });
+
+    return Scaffold(
+      body: Center(
+        child: FlutterLogo(size: 200),
+      ),
+    );
+  }
+}
