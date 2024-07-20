e0f00fa9fb02c9cc7bed44db746683dc75565ec4
crap1nyk <nv06645@gmail.com>
2024-07-21 03:44:15 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  void _login() async {
+  void _login(BuildContext context) async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _emailController.text,
@@ -84,40 +84,157 @@ class _LoginScreenState extends State<LoginScreen> {
       );
     }
   }
+  
+  void _forgotPassword() {
+    print("Forgot Password clicked");
+    // Handle forgot password logic
+  }
 
-  @override
+  void _loginWithGoogle() {
+    print("Login with Google clicked");
+    // Handle login with Google logic
+  }
+
+  void _loginWithFacebook() {
+    print("Login with Facebook clicked");
+    // Handle login with Facebook logic
+  }
+
+      @override
   Widget build(BuildContext context) {
+    // Obtain the height of the screen
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+      body: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: screenHeight, // Ensure the content takes full height
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: _login,
-                  child: Text('Login'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(height: 50),
+                Image.asset(
+                  'assets/images/logo.jpg', // Replace with your image asset path
+                  height: 100,
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 20),
+                Text(
+                  'Log In',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Montserrat', // Change to your preferred font
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Enter your details below to log in',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Montserrat', // Change to your preferred font
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: const Color.fromARGB(179, 0, 0, 0)),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 246, 245, 245),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  style: TextStyle(color: const Color.fromARGB(255, 6, 4, 4)),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: const Color.fromARGB(179, 0, 0, 0)),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 246, 245, 245),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  obscureText: true,
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                  onTap: _forgotPassword,
+                  child: Text(
+                    'Forgot Password?',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 216, 71, 209),
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
                 ElevatedButton(
-                  onPressed: _signUp,
-                  child: Text('Sign Up'),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color.fromARGB(255, 180, 83, 180),
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onPressed: () => _login(context),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'or login with',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: _loginWithGoogle,
+                      child: Image.asset(
+                        'assets/images/R.png', // Google logo asset path
+                        height: 40,
+                      ),
+                    ),
+                    SizedBox(width: 30),
+                    GestureDetector(
+                      onTap: _loginWithFacebook,
+                      child: Image.asset(
+                        'assets/images/facebook-logo-0.png', // Facebook logo asset path
+                        height: 40,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
